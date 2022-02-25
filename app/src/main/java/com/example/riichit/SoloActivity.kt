fa12040b05470e4279e1ebe5bc514a6f858e932b
N1ckn1ght <n1ckn1ght@protonmail.com>
2022-02-25 20:57:42 +0800

    solo mahjong hand WIP
    changed linearlayout to constraint; implemented minor changes in creation logic; 2 layout TODOs + must be clickable

--- app/src/main/java/com/example/riichit/SoloActivity.kt
+++ app/src/main/java/com/example/riichit/SoloActivity.kt
@@ -8,22 +8,47 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class SoloActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solo)
 
-        val tiles = mutableListOf(R.drawable.dragon_red, R.drawable.dragon_white, R.drawable.dragon_green)
-        // val hand = mutableListOf(1, 1, 1)
+        val hand = makeHand()
+        var showHand = transform(hand)
 
         val rhand = findViewById<RecyclerView>(R.id.hand)
         Log.d("stage", "rhand was found.")
         val handAdapter = HandAdapter(LayoutInflater.from(this))
         Log.d("stage", "handAdapter was created.")
-        handAdapter.submitList(tiles)
+        handAdapter.submitList(showHand)
         Log.d("stage", "list of tiles was submitted.")
         rhand.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         Log.d("stage", "linear layout manager has been accepted.")
         rhand.adapter = handAdapter
         Log.d("stage", "adapter has been passed successfully!")
+    }
+
+    private fun makeHand() : Array<Int> {
+        val hand = Array<Int>(34){0}
+        hand[0] = 3
+        hand[1] = 1
+        hand[2] = 1
+        hand[3] = 1
+        hand[4] = 1
+        hand[5] = 1
+        hand[6] = 1
+        hand[7] = 1
+        hand[8] = 3
+        return hand
+    }
+
+    private fun transform(hand: Array<Int>) : MutableList<Int> {
+        var showHand = mutableListOf<Int>()
+        for (i in hand.indices) {
+            for (j in 1..hand[i]) {
+                showHand.add(i)
+            }
+        }
+        return showHand
     }
 }
