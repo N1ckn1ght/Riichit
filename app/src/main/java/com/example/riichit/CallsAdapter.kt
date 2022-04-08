982f1afedfa7d9fbc606b3d91758cd3d156a5567
N1ckn1ght <n1ckn1ght@protonmail.com>
2022-04-08 19:15:43 +0800

    full field interface

---
+++ app/src/main/java/com/example/riichit/CallsAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.riichit
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+class CallsAdapter(private val inflater: LayoutInflater, private val context: Context,
+                     private val width: Int, private val height: Int, private val padding: Int) : ListAdapter<Int, DiscardViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiscardViewHolder {
+        val row: View = inflater.inflate(R.layout.tile, parent, false)
+        parent.setPadding(0, padding, 0, padding)
+        return DiscardViewHolder(row, 136)
+    }
+
+    override fun onBindViewHolder(holder: DiscardViewHolder, position: Int) {
+        holder.iv.layoutParams.width = width
+        holder.iv.layoutParams.height = height
+        if ((position + 1) % 4 == 0) {
+            holder.iv.setMargin(0, 0, padding, 0)
+        }
+        holder.bindTo((context as SoloActivity).tiles[getItem(position) / 4], getItem(position))
+    }
+
+    companion object {
+        private val DIFF_CALLBACK: DiffUtil.ItemCallback<Int> = object : DiffUtil.ItemCallback<Int>() {
+            override fun areItemsTheSame(oldTile: Int, newTile: Int): Boolean {
+                return oldTile == newTile
+            }
+            override fun areContentsTheSame(oldTile: Int, newTile: Int): Boolean {
+                return areItemsTheSame(oldTile, newTile)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/riichit/SoloActivity.kt
+++ app/src/main/java/com/example/riichit/SoloActivity.kt
@@ -9,7 +9,6 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,10 +17,7 @@ import java.util.Random
 
 class SoloActivity : AppCompatActivity() {
     internal lateinit var tiles: Array<Int>
-    internal var kanStatus = 0
-    internal var riichiStatus = 0
-    internal var tsumoStatus = 0
-    private lateinit var wall: Array<Int>
+
     private lateinit var rtsumo: ImageView
     private lateinit var rhand: RecyclerView
     private lateinit var rdiscard: RecyclerView
@@ -30,19 +26,28 @@ class SoloActivity : AppCompatActivity() {
     private lateinit var handAdapter: HandAdapter
     private lateinit var discardAdapter: DiscardAdapter
     private lateinit var indicatorAdapter: DiscardAdapter
-    private lateinit var callsAdapter: DiscardAdapter
+    private lateinit var callsAdapter: CallsAdapter
+
+    private var kanStatus = 0
+    private var riichiStatus = 0
+    private var tsumoStatus = 0
     private var size: Int = 136
-    private var tilesLeft: Int = 80
+    private var tilesLeft: Int = 18
     private var tsumo: Int = 136
-    private lateinit var hand: MutableList<Int>
-    private lateinit var computableHand: Array<Int>
-    private lateinit var discard: MutableList<Int>
     private var openDoras: Int = 1
+
+    private lateinit var all: Array<Int>
+    private lateinit var wall: Array<Int>
+    private lateinit var hand: MutableList<Int>
+    private lateinit var discard: MutableList<Int>
     private lateinit var indicator: Array<Int>
     private lateinit var showableIndicator: MutableList<Int>
+    private lateinit var calls: MutableList<Int>
     private lateinit var kanButton: Button
     private lateinit var riichiButton: Button
     private lateinit var tsumoButton: Button
+
+    private lateinit var computableHand: Array<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,6 +86,8 @@ class SoloActivity : AppCompatActivity() {
         val padding = (displayMetrics.widthPixels * 0.016).toInt()
         val discardTileHeight = (min((displayMetrics.heightPixels - handTileHeight - padding * 4), handTileHeight * 3) / 3.0).toInt()
         val discardTileWidth = (min((displayMetrics.heightPixels - handTileHeight - padding * 4), handTileHeight * 3) / 4.0).toInt()
+        val kanTileWidth = (handTileWidth * 3 - padding) / 4
+        val kanTileHeight = kanTileWidth * 4 / 3
         Log.d("d/screen", "display metrics: ${displayMetrics.widthPixels}x${displayMetrics.heightPixels}, hand tile sizes: ${handTileWidth}x${handTileHeight}, padding: ${padding}, discard tile sizes: ${discardTileWidth}x${discardTileHeight}")
 
         rtsumo.layoutParams.width = handTileWidth
@@ -92,15 +99,17 @@ class SoloActivity : AppCompatActivity() {
         rindicator.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         indicatorAdapter = DiscardAdapter(LayoutInflater.from(this), this, handTileWidth, handTileHeight, padding)
         rcalls.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        callsAdapter = DiscardAdapter(LayoutInflater.from(this), this, handTileWidth, handTileHeight, 0)
+        callsAdapter = CallsAdapter(LayoutInflater.from(this), this, kanTileWidth, kanTileHeight, padding)
 
         kanButton.setMargin(0, padding, padding, 0)
         riichiButton.setMargin(0, 1, padding, 0)
         tsumoButton.setMargin(0, 1, padding, 0)
 
-        wall = Array(size){ i -> i }
+        all = Array(size){ i -> i }
+        wall = Array(tilesLeft){ 136 }
         discard = mutableListOf()
         indicator = Array(14){size}
+        calls = mutableListOf()
         for (i in 0..13) {
             indicator[i] = randomTile()
         }
@@ -108,6 +117,9 @@ class SoloActivity : AppCompatActivity() {
         hand = MutableList(13){size}
         for (i in 0..12) {
             hand[i] = randomTile()
+        }
+        for (i in 0 until tilesLeft) {
+            wall[i] = randomTile()
         }
         hand.sort()
         handAdapter.submitList(hand)
@@ -123,11 +135,14 @@ class SoloActivity : AppCompatActivity() {
             rtsumo.performClick()
             return
         }
-        if (riichiStatus > 0) {
+        if (riichiStatus == 2) {
             Toast.makeText(baseContext, baseContext.getString(R.string.illegal_discard), Toast.LENGTH_SHORT).show()
             return
         }
-        if (tilesLeft > 0) {
+        if (riichiStatus == 1) {
+            setRiichi()
+        }
+        if (tilesLeft > openDoras - 1) {
             if (kanStatus > 0) {
                 var count = 0
                 for (it in hand) {
@@ -166,7 +181,8 @@ class SoloActivity : AppCompatActivity() {
                 hand = hand.filterNot {
                     it / 4 == tsumo / 4
                 } as MutableList<Int>
-                getFromDead()
+                getFromDead(toRemove / 4)
+                return
             }
             handAdapter.submitList(hand)
             rhand.adapter = handAdapter
@@ -175,10 +191,9 @@ class SoloActivity : AppCompatActivity() {
     }
 
     fun onClickTsumo(view: View) {
-        if (tilesLeft > 0) {
+        if (tilesLeft > openDoras - 1) {
             if (riichiStatus == 1) {
-                riichiStatus = 2
-                riichiButton.disable()
+                setRiichi()
             }
             if (kanStatus > 0) {
                 var handCopy = hand
@@ -190,36 +205,41 @@ class SoloActivity : AppCompatActivity() {
                         // TODO: check for Riichi, if this ever possible
                     }
                     hand = handCopy
-                    handAdapter.submitList(hand)
-                    rhand.adapter = handAdapter
-                    getFromDead()
+                    getFromDead(tsumo / 4)
                 } else {
                     Toast.makeText(baseContext, baseContext.getString(R.string.illegal_kan), Toast.LENGTH_SHORT).show()
-                    return
                 }
             } else {
                 discard.add(tsumo)
                 discardAdapter.submitList(discard)
                 rdiscard.adapter = discardAdapter
-                tsumo = randomTile()
+                tilesLeft--
+
+                if (tilesLeft > openDoras - 1) {
+                    tsumo = wall[tilesLeft]
+                    rtsumo.setImageResource(tiles[tsumo / 4])
+                } else {
+                    tsumoButton.disable()
+                    tsumo = 136
+                    rtsumo.setImageResource(android.R.color.transparent)
+                    onGameOver()
+                }
             }
-            tilesLeft--
-            rtsumo.setImageResource(tiles[tsumo / 4])
-            return
+        } else {
+            Toast.makeText(baseContext, baseContext.getString(R.string.no_tiles_left), Toast.LENGTH_SHORT).show()
         }
-        // TODO: finish the game
-        Toast.makeText(baseContext, baseContext.getString(R.string.no_tiles_left), Toast.LENGTH_SHORT).show()
     }
 
     fun onCallKan(view: View) {
         if (kanStatus > 0) {
             kanStatus = 0
             kanButton.text = getString(R.string.button_kan)
-            riichiButton.enable()
+            if (riichiStatus < 1) {
+                riichiButton.enable()
+            }
             tsumoButton.enable()
         } else {
-            if (tilesLeft > 0) {
-                tilesLeft--
+            if (tilesLeft > openDoras - 1) {
                 kanStatus = 1
                 kanButton.text = getString(R.string.button_cancel)
                 riichiButton.disable()
@@ -231,14 +251,17 @@ class SoloActivity : AppCompatActivity() {
     }
 
     fun onCallRiichi(view: View) {
-        // fix needed; 0 - no riichi, 1 - it's called (drop from anywhere), 2 - it's in progress.
         if (riichiStatus > 0) {
             riichiStatus = 0
             riichiButton.text = getString(R.string.button_riichi)
+            kanButton.enable()
+            tsumoButton.enable()
         } else {
-            if (tilesLeft > 0) {
+            if (tilesLeft > openDoras - 1) {
                 riichiStatus = 1
                 riichiButton.text = getString(R.string.button_cancel)
+                kanButton.disable()
+                tsumoButton.disable()
                 // TODO: implement actual Riichi logic
             } else {
                 Toast.makeText(baseContext, baseContext.getString(R.string.no_tiles_left), Toast.LENGTH_SHORT).show()
@@ -247,7 +270,12 @@ class SoloActivity : AppCompatActivity() {
     }
 
     fun onCallTsumo(view: View) {
-        // TODO: calc the hand!!
+        tsumoStatus = 1
+        onGameOver()
+    }
+
+    private fun onGameOver() {
+
     }
 
     private fun randomTile() : Int {
@@ -258,37 +286,45 @@ class SoloActivity : AppCompatActivity() {
         val rnd: Int = Random().nextInt(size)
         size--
         if (size > rnd) {
-            wall[size] = wall[rnd] + wall[size]
-            wall[rnd] = wall[size] - wall[rnd]
-            wall[size] = wall[size] - wall[rnd]
+            all[size] = all[rnd] + all[size]
+            all[rnd] = all[size] - all[rnd]
+            all[size] = all[size] - all[rnd]
         }
-        return wall[size]
+        return all[size]
     }
 
-    private fun getFromDead() {
-        var temp = tsumo
+    private fun getFromDead(kanItem: Int) {
         tsumo = indicator[0]
-        indicator[0] = temp
         showableIndicator[openDoras] = indicator[5 + openDoras * 2]
         openDoras++
         indicatorAdapter.submitList(showableIndicator)
         rindicator.adapter = indicatorAdapter
-
         kanStatus = 0
         kanButton.text = getString(R.string.button_kan)
         riichiButton.enable()
+        tsumoButton.enable()
+        handAdapter.submitList(hand)
+        rhand.adapter = handAdapter
+        calls.add(136)
+        calls.add(kanItem * 4 + 1)
+        calls.add(kanItem * 4 + 2)
+        calls.add(136)
+        callsAdapter.submitList(calls)
+        rcalls.adapter = callsAdapter
+        rtsumo.setImageResource(tiles[tsumo / 4])
+    }
+
+    private fun setRiichi() {
+        riichiStatus = 2
+        riichiButton.disable()
+        riichiButton.text = getString(R.string.button_riichi)
+        kanButton.enable()
         tsumoButton.enable()
     }
 
     private fun transform(hand: MutableList<Int>) : Array<Int> {
         // this function should transform hand from view variation to computable
         return arrayOf()
-    }
-
-    private fun View.setMargin(left: Int, top: Int, right: Int, bottom: Int) {
-        val p = this.layoutParams as ConstraintLayout.LayoutParams
-        p.setMargins(left, top, right, bottom)
-        this.layoutParams = p
     }
 
     private fun Button.enable() {
@@ -303,10 +339,5 @@ class SoloActivity : AppCompatActivity() {
         this.isClickable = false
         this.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.white))
         this.setTextColor(ContextCompat.getColor(baseContext, R.color.gray))
-    }
-
-    private fun min(a: Int, b: Int) : Int {
-        if (a > b) return b
-        return a
     }
 }
